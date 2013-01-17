6ccd70f9e282bf7708e2b64496c8865950de9908
daipeng <daipeng123456@gmail.com>
2013-01-17 15:43:55 +0800

    fix

---
+++ wiki/model.py
@@ -0,0 +1,49 @@
+import web
+
+db = web.database(dbn='sqlite3', db='sql/main.db')
+
+def get_pages():
+    return db.select('pages', order='id DESC')
+
+def get_page_by_url(url):
+    """
+    
+    Arguments:
+    - `url`:
+    """
+    try:
+        return db.select('pages', where='url=$url', vars=locals())[0]
+    except IndexError:
+        return None
+
+def get_page_by_id(id):
+    """
+    
+    Arguments:
+    - `id`:
+    """
+    try:
+        return db.select('pages', where='id=$id', vars=locals())[0]
+    except IndexError:
+        return None
+
+def new_page(url, title, text):
+    db.insert('pages', url=url, title=title, content=text)
+    
+def del_page(id):
+    """
+    
+    Arguments:
+    - `id`:
+    """
+    db.delete('pages', where='id=$id', vars=locals())
+
+def update_page(id, url, title, text):
+    """
+    
+    Arguments:
+    - `id`:
+    - `url`:
+    
+    """
+    db.update('pages', where='id=$id', vars=locals(), url=url, title=title, content=text)
--- wiki/wiki.py
+++ wiki/wiki.py
@@ -33,7 +33,68 @@ class Page:
 
 class New:
     
+    def not_page_exists(url):
+        """
+        Arguments:
+        - `url`:
+        """
+        return not bool(model.get_page_by_url(url))
+    
+    page_exists_validator = web.form.Validator('Page already exists', not_page_exists)
+
+    form = web.form.Form(
+        web.form.Textbox('url', web.form.notnull, size=30, description="Location:"),
+        web.form.Textbox('title', web.form.notnull, size=30, description="Page Title:"),
+        web.form.Textarea('content', web.form.notnull, rows=30, cols=80, description="Page content:", post="use markdown syntax"),
+        web.form.Button('Create Page')
+        )
     def GET(self):
+        url = web.input(url='').url
+        form = self.form()
+        return render.new(form)
+    
+    def POST(self):
+        form = self.form()
+        if not form.validates():
+            return render.new(form)
+        model.new_page(form.d.url, form.d.title, form.d.content)
+        raise web.seeother('/' + form.d.url)
+
+class Delete:
+    """
+    
+    """
+    
+    def POST(self, id):
+        model.del_page(int(id))
+        raise web.seeother('/')
+
+class Edit:
+    form = web.form.Form(
+        web.form.Textbox('url',web.form.notnull, description="Location:"),
+        web.form.Textbox('title', web.form.notnull, size=30, description="Page Title:"),
+        web.form.Textarea('content', web.form.notnull, rows=30, cols=80, description="Page content:", post="use markdown syntax"),
+        web.form.Button('Update Page')
+        )
+    def GET(self, id):
+        page = model.get_page_by_id(int(id))
+        form = self.form()
+        form.fill(page)
+        return render.edit(page, form)
+
+    def POST(self, id):
+        form = self.form()
+        page = model.get_page_by_id(int(id))
+        if not form.validates():
+            return render.edit(page, form)
+        model.update_page(int(id), form.d.url, form.d.title, form.d.content)
+        raise web.seeother('/')
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
+
+         
         
-        import time
-        import re
+        

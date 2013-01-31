9a784785a207279f7000e4ce821a1526fd86947c
daipeng <daipeng123456@gmail.com>
2013-01-31 16:26:12 +0800

    fix

--- jinja2_template_try.py
+++ jinja2_template_try.py
@@ -3,8 +3,10 @@
 
 from jinja2 import *
 import sys,traceback
+import time
 
-def fileSystem_try(file, path, **kargs):
+def fileSystem_try(f, path, **kargs):
+
     env = Environment(loader=FileSystemLoader(path)))
     try:
         template = env.get_template(file)
@@ -16,4 +18,14 @@ def fileSystem_try(file, path, **kargs):
         print "tempalte %s under %s is error" % (file, path)
         print traceback.print_stack()
         
+if __name__ == "__main__":
+    path = raw_input('please entry the template dir path:')
+    f = raw_input('please enter the file name:')
+    b = raw_input('if write the result to file:')
+    while b not in ['Y', 'N', 'y', 'n']:
+        b = raw_input('you must entry y or n:')
 
+    result = fileSystem_try(f, path)
+    if b in ['Y', 'y']:
+        tmp = '/tmp/%s' % (time.now()+f)
+    open(tmp, 'w').write(result)
